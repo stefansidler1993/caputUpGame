882c4d8997687fce7da9fba802942c90842e425a
Stefan Sidler <stefan_sidler1993@hotmail.es>
2019-01-19 12:12:27 -0400

    json input updated

--- app/src/main/java/com/electronic/lapsus/caputup/CategoriesService.java
+++ app/src/main/java/com/electronic/lapsus/caputup/CategoriesService.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface CategoriesService {
 
-    String API_ROUTE = "/bins/8qey0";
+    String API_ROUTE = "/bins/m8xrc";
 
     @GET(API_ROUTE)
     Call<List<Category>> getCategories();
